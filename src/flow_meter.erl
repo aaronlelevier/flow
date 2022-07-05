401a2613f7d317fedc7f7a1e49d2026b44dcc71e
aaronlelevier <aaron.lelevier@gmail.com>
2022-07-05 08:39:39 -0700

    flow_meter - first commit - initial framing of gph config and state

--- src/flow_meter.erl
+++ src/flow_meter.erl
@@ -4,14 +4,16 @@
 %%% @end
 %%% Created : 09. Jun 2022 6:13 AM
 %%%-------------------------------------------------------------------
--module(flow_single).
+-module(flow_meter).
 -author("Aaron Lelevier").
 
 -behaviour(gen_statem).
 
 %% API
 -export([
-  start/0
+  start/1,
+  open/3,
+  get_data/1
 ]).
 
 %% gen_statem callbacks
@@ -19,7 +21,8 @@
   init/1,
   terminate/3,
   code_change/4,
-  callback_mode/0
+  callback_mode/0,
+  handle_event/3
 ]).
 
 -record(state, {}).
@@ -28,15 +31,25 @@
 %%% API
 %%%===================================================================
 
-start() ->
-  gen_statem:start(?MODULE, [], []).
+start(Config) ->
+  gen_statem:start(?MODULE, Config, []).
+
+get_data(Pid) ->
+  gen_statem:call(Pid, get_data).
 
 %%%===================================================================
 %%% gen_statem callbacks
 %%%===================================================================
 
-init([]) ->
-  {ok, idle, #state{}}.
+init(Config) ->
+  State = open,
+  Data = #{
+    config => Config,
+    state => #{
+      gph => 0
+    }
+  },
+  {ok, State, Data}.
 
 -spec callback_mode() -> handle_event_function | state_functions.
 callback_mode() ->
@@ -47,6 +60,17 @@ terminate(_Reason, _StateName, _State = #state{}) ->
 
 code_change(_OldVsn, StateName, State = #state{}, _Extra) ->
   {ok, StateName, State}.
+
+%% State callbacks
+open(EventType, EventContent, Data) ->
+  handle_event(EventType, EventContent, Data).
+
+%% Handle events common to all states
+handle_event({call, From}, get_data, Data) ->
+  {keep_state, Data, [{reply, From, Data}]};
+handle_event(_, _, Data) ->
+  %% Ignore all other events
+  {keep_state, Data}.
 
 %%%===================================================================
 %%% Internal functions
